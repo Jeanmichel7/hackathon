4cc7be8b9c098060b5b507676ec875b381d020a6
Jean-michel Rasser <jrasser@k2r3p7.42mulhouse.fr>
2022-11-08 19:02:01 +0100

    test input

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const http = axios.create({
   },
 })
 
-let address = "0xB8c9627627a6F1F78CD2b9d172A2816529F313B8";
+let address = "";
+// let address = "0xB8c9627627a6F1F78CD2b9d172A2816529F313B8";
 let scAddress = "0xCc7bb2D219A0FC08033E130629C2B854b7bA9195";
 // let address = "0xa22d224bFCBBB2aD6714C2b5feBbfe638fBC9627"
 let network = "ethereum-goerli";
@@ -31,8 +32,10 @@ async function getWallet() {
 
 async function getWalletBalance() {
   try {
+    let thomas = document.getElementById("wallet").value;
+    address = thomas;
     const response = await http.get('/wallet/' + address + '/balance');
-    console.log("res getWalletBalance : ", response.data);
+    console.log("res getWalletBalance : ", response.data.balance.formatted);
   }
   catch (error) {
     console.error(error);
@@ -41,6 +44,7 @@ async function getWalletBalance() {
 
 async function getWalletNetworkBalance() {
   try {
+    
     const response = await http.get('/wallet/' + address + '/' + network + '/balance');
     console.log("res getWalletNetworkBalance : ", response.data.balance.formatted);
   }
@@ -90,13 +94,23 @@ async function getScFunction() {
 }
 
 
+// document
+//   .getElementById("wallet")
+//   .addEventListener("input", function(e) {
+//     document .getElementById("display-address").innerText = e.target.value;
+//     address = e.target.value;
+// });
+
+
+
 // getWalletNetworkBalance();
 
 // getWalletBalance();
 // getWallet();
 // getAllSc();
 // getSC();
-getScFunction();
+// getScFunction();
 
 
 // getWalletNetworkBalance();
+
