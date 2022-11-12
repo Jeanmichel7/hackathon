e34de8363e07682f55e1742d9585a92a9fdc73dd
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-11-12 13:38:38 +0100

    fix multi post create token

--- views/tokenPage/tokenPage.js
+++ views/tokenPage/tokenPage.js
@@ -34,16 +34,19 @@ async function displayTemplate() {
   return allTemplate;
 }
 
-async function displayForm (template) {
+function displayForm (template) {
   for(let elem in template) {
     let str = "btn-form-create-token" + elem;
     const btnForm = document.getElementById(str);
-    btnForm.addEventListener("click", async () => {
-      await displayFormCreateToken(template, elem);
+    btnForm.addEventListener("click", () => {
+      displayFormCreateToken(template, elem);
       return elem;
     });
   }
 }
+
+
+
 
 async function displayFormCreateToken(templates, index) {
   document.getElementById("display-form-create-token").innerHTML = 
@@ -68,13 +71,35 @@ async function displayFormCreateToken(templates, index) {
           <label for="token-supply">Token supply</label>
           <input type="number" class="form-control" id="token-supply" placeholder="Enter token supply">
         </div>
-        <button type="button" id="btn-create-token" class="btn btn-primary" 
+        <button type="button" id="btn-create-token${index}" class="btn btn-primary" 
         data-bs-toggle="modal" data-bs-target="#modal-create-token">
           Create token
         </button>
       </form>
     </div>
   </div>
+
+  <!-- Modal -->
+    <div class="modal fade" id="modal-create-token" tabindex="-1" aria-labelledby="modal-create-tokenLabel"
+    aria-hidden="false">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="modal-create-tokenLabel">Confirm</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>You are about to create an ERC-20 token</p>
+            <p>Gas fee : <span id="modal-gas-fee"></span></p>
+            <p>Are you sure you want to continue?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" id="valid-create-token" class="btn btn-primary" data-bs-dismiss="modal">Continue</button>
+          </div>
+        </div>
+      </div>
+    </div>
   `
   const btnValideCreateToken = document.getElementById("valid-create-token");
   btnValideCreateToken.addEventListener("click", async () => {
@@ -96,17 +121,18 @@ async function displayFormCreateToken(templates, index) {
 }
 
 
-
-
 async function validateCreatToken(templates, elem) {
   const modelGasFee = document.getElementById("modal-gas-fee");
   let gasFee = await gasOnGoerli();
   console.log("gasFee : ", gasFee);
 
   modelGasFee.innerHTML = gasFee.average.maxFeePerGas;
+
   let ret = createToken(templates[elem].id);
   return ret;
+  // console.log("creation token : ", templates[elem].id);
 }
+
 
 
 
